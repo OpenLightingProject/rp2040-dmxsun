7efdb8fa8b58a270cb1fd26468817936c5847804
Jannis Achstetter <kripton@kripserver.net>
2021-03-30 09:48:54 +0200

    Fix: Make shared fields static to enable compilation with gcc 10

--- src/dmahandler.h
+++ src/dmahandler.h
@@ -7,8 +7,8 @@
 
 #define WAVETABLE_LENGTH 5648   // bits per DMX packet. Wavetable has 16*this bits in total
 
-int dma_chan;                     // The DMA channel we use to push data around
-uint8_t dmx_values[16][512];      // 16 universes with 512 byte each
+static int dma_chan;                     // The DMA channel we use to push data around
+static uint8_t dmx_values[16][512];      // 16 universes with 512 byte each
 static uint16_t wavetable[WAVETABLE_LENGTH];  // 16 universes (data type) with 5648 bit each
 
 void wavetable_write_bit(int universe, uint16_t* bitoffset, uint8_t value);
