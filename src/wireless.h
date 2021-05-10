24497d9cca3900866a96721c4ddc260910f494f4
Jannis Achstetter <kripton@kripserver.net>
2021-05-10 21:11:31 +0200

    Prepare a spectrum analyzer (wireless scanner)

--- src/wireless.h
+++ src/wireless.h
@@ -9,12 +9,16 @@
 #include <RF24Network.h>
 #include <RF24Mesh.h>
 
+// nRF24L01+ can tune to 128 channels with 1 MHz spacing from 2400MHz to 2527MHz
+#define MAXCHANNEL 128
+
 class Wireless {
   public:
     void init();
     void cyclicTask();
 
   private:
+    uint16_t signalStrength[MAXCHANNEL]; // Used for spectrum analyser mode
 };
 
 #endif // __cplusplus
