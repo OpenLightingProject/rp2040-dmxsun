1732844fa96abe369192f899eae8d3c1ebb09e6f
Jannis Achstetter <kripton@kripserver.net>
2021-05-11 22:49:33 +0200

    Use Deflate compression for non-ssi files in www folder to save flash space

--- regen-fsdata.sh
+++ regen-fsdata.sh
@@ -3,9 +3,11 @@
 if [ ! -f makefsdata ]; then
     # Doing this outside cmake as we don't want it cross-compiled but for host
     echo Compiling makefsdata
-    gcc -o makefsdata -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c
+    #gcc -o makefsdata -DMAKEFS_SUPPORT_BROTLI -lbrotlienc -lbrotlidec -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c
+    gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZLIB=1 -lz -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c
 fi
 
 echo Regenerating fsdata.c
-./makefsdata www -11 -f:src/fsdata.c
+#./makefsdata www -brotli:11 -11 -f:src/fsdata.c
+./makefsdata www -defl:9 -11 -f:src/fsdata.c
 echo Done
