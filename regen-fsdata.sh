f1ab181874087c17353ffde34f428626db19712a
Jannis Achstetter <kripton@kripserver.net>
2021-05-11 23:06:18 +0200

    Fix regen-fsdata.sh for Ubuntu's picky ld/gcc wrt to position of cli parameters ...

--- regen-fsdata.sh
+++ regen-fsdata.sh
@@ -4,7 +4,7 @@ if [ ! -f makefsdata ]; then
     # Doing this outside cmake as we don't want it cross-compiled but for host
     echo Compiling makefsdata
     #gcc -o makefsdata -DMAKEFS_SUPPORT_BROTLI -lbrotlienc -lbrotlidec -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c
-    gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZLIB=1 -lz -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c
+    gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZLIB=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz
 fi
 
 echo Regenerating fsdata.c
