87336683c96e5b94203eaf4c8105bc23bb1e7e13
Jannis Achstetter <kripton@kripserver.net>
2021-05-18 22:43:07 +0200

    Fix regen-fsdata to work in GitHub Actions, again (Zopfli headers are in subdir on Ubuntu)

--- regen-fsdata.sh
+++ regen-fsdata.sh
@@ -6,10 +6,10 @@ if [ ! -f makefsdata ]; then
     echo Compiling makefsdata
     if [ -z "$CI" ]; then
         ## Development system, use the blazing fast zlib
-        gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZLIB=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz
+        gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZLIB=1 -Ilib/lwip/src/include -I/usr/include/zopfli -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz
     else
         ## CI/Release build: Use the slower but more effective zopfli algorithm
-        gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZOPFLI=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz -lzopfli
+        gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZOPFLI=1 -Ilib/lwip/src/include -I/usr/include/zopfli -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz -lzopfli
     fi
 fi
 
