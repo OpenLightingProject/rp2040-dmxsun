7fb04a5ed493d78a5151c2e59776e91188535697
Jannis Achstetter <kripton@kripserver.net>
2021-05-18 22:35:26 +0200

    Fix regen-fsdata to work in GitHub Actions

--- regen-fsdata.sh
+++ regen-fsdata.sh
@@ -4,21 +4,21 @@
 if [ ! -f makefsdata ]; then
     # Doing this outside cmake as we don't want it cross-compiled but for host
     echo Compiling makefsdata
-    if [ "$CI" == "true" ]; then
-        # Use the slower but more effective zopfli algorithm
-        gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZOPFLI=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz -lzopfli
-    else
-        # During development, just use the blazing fast zlib
+    if [ -z "$CI" ]; then
+        ## Development system, use the blazing fast zlib
         gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZLIB=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz
+    else
+        ## CI/Release build: Use the slower but more effective zopfli algorithm
+        gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZOPFLI=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz -lzopfli
     fi
 fi
 
 echo Regenerating fsdata.c ...
-if [ "$CI" == "true" ]; then
-    # Use the slower but more effective zopfli algorithm
-    ./makefsdata www -defl:400 -11 -f:src/fsdata.c
-else
-    # During development, just use the blazing fast zlib
+if [ -z "$CI" ]; then
+    ## Development system, use the blazing fast zlib
     ./makefsdata www -defl:6 -11 -f:src/fsdata.c
+else
+    ## CI/Release build: Use the slower but more effective zopfli algorithm
+    ./makefsdata www -defl:400 -11 -f:src/fsdata.c
 fi
 echo Done
