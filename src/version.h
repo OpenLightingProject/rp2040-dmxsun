593341411342db24abcfbd4b0ac4ef61d60f9165
Jannis Achstetter <kripton@kripserver.net>
2021-05-18 23:17:23 +0200

    Version 0.2.0

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define VERSION "0.1.0"
+#define VERSION "0.2.0"
 
 // Version number reported in the USB device descriptor
 // Since no one cares about "bug fixes" there, we report first and second
 // digit of VERSION here
-#define VERSION_BCD 0x0001
+#define VERSION_BCD 0x0002
