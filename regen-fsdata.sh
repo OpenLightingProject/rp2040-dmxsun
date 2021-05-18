cdc8fd449800c2b27a6cbda3002908d31a8f0fb0
Jannis Achstetter <kripton@kripserver.net>
2021-05-18 22:20:12 +0200

    Use zlib (or zopfli on CI) to compress the www data for the web server

--- regen-fsdata.sh
+++ regen-fsdata.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+# If the makefsdata tool shipped with lwip hasn't been compiled, do so now
 if [ ! -f makefsdata ]; then
     # Doing this outside cmake as we don't want it cross-compiled but for host
     echo Compiling makefsdata
-    #gcc -o makefsdata -DMAKEFS_SUPPORT_BROTLI -lbrotlienc -lbrotlidec -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c
-    gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZLIB=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz
+    if [ "$CI" == "true" ]; then
+        # Use the slower but more effective zopfli algorithm
+        gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZOPFLI=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz -lzopfli
+    else
+        # During development, just use the blazing fast zlib
+        gcc -o makefsdata -DMAKEFS_SUPPORT_DEFLATE=1 -DMAKEFS_SUPPORT_DEFLATE_ZLIB=1 -Ilib/lwip/src/include -Isrc lib/lwip/src/apps/http/makefsdata/makefsdata.c -lz
+    fi
 fi
 
-echo Regenerating fsdata.c
-#./makefsdata www -brotli:11 -11 -f:src/fsdata.c
-./makefsdata www -defl:9 -11 -f:src/fsdata.c
+echo Regenerating fsdata.c ...
+if [ "$CI" == "true" ]; then
+    # Use the slower but more effective zopfli algorithm
+    ./makefsdata www -defl:400 -11 -f:src/fsdata.c
+else
+    # During development, just use the blazing fast zlib
+    ./makefsdata www -defl:6 -11 -f:src/fsdata.c
+fi
 echo Done
