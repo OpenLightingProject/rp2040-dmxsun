c13f295230aea097ec610de37c1cf31faaf48a7b
Jannis Achstetter <kripton@kripserver.net>
2021-10-09 00:34:10 +0200

    Version 0.8.0

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define VERSION "0.3.0"
+#define VERSION "0.8.0"
 
 // Version number reported in the USB device descriptor
 // Since no one cares about "bug fixes" there, we report first and second
 // digit of VERSION here, which means we are limited to 255 per digit :(
-#define VERSION_BCD 0x0003
+#define VERSION_BCD 0x0008
