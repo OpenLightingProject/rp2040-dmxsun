77968edba01c9d2be8395c4394df154768cddae8
Jannis Achstetter <kripton@kripserver.net>
2022-06-25 11:54:48 +0200

    Update react from 17.x to 18.2.0. Fixes #46

--- wwwsrc/src/index.js
+++ wwwsrc/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
