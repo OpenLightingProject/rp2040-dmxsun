5cb9ce62647e78107cb1b0cc49c6a3ea5e5385fb
Jannis Achstetter <kripton@kripserver.net>
2023-02-04 19:37:47 +0100

    Shove off about 21kByte off the final binary by using PurgeCSS

--- wwwsrc/postcss.config.js
+++ wwwsrc/postcss.config.js
@@ -1,7 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
     plugins: [
         require('cssnano')({
             preset: 'advanced',
         }),
+        purgecss({
+            content: ['build/static/js/*.js']
+          })
     ],
 };
